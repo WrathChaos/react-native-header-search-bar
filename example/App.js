3b782dc06f2123f6766cc05fe6d69d09d0970ea0
FreakyCoder <kurayogun@gmail.com>
2019-10-09 10:31:37 +0300

    Example is updated and library is  tested

--- example/App.js
+++ example/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { View, StatusBar } from "react-native";
 
-import HeaderSearchBar from "./lib/src/HeaderSearchBar";
+import HeaderSearchBar from "react-native-header-search-bar";
 
 const App = () => {
   return (
     <View>
       <StatusBar barStyle="dark-content" />
-      <HeaderSearchBar />
+      <HeaderSearchBar onChangeText={text => console.log(text)} />
     </View>
   );
 };
